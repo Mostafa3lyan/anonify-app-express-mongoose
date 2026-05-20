52f40cfa597ea956ed52cbf629dff79d94ccbd41
Mostafa3lyan <96500374+Mostafa3lyan@users.noreply.github.com>
2026-05-21 02:05:09 +0300

    chore: remove rate limit

--- src/app.bootstrap.js
+++ src/app.bootstrap.js
@@ -4,17 +4,15 @@ import {
   ErrorException,
   globalErrorHandling,
   NotFoundException,
-  sendEmail,
-  successResponse,
+  successResponse
 } from "./common/utils/index.js";
 
 import cors from "cors";
 import express from "express";
+import helmet from "helmet";
 import { resolve } from "node:path";
 import { authenticationDB, connectRedis } from "./DB/index.js";
 import { authRouter, messageRouter, userRouter } from "./modules/index.js";
-import helmet from "helmet";
-import rateLimit, { ipKeyGenerator } from "express-rate-limit";
 
 async function bootstrap() {
   const app = express();
@@ -25,28 +23,28 @@ async function bootstrap() {
     optionsSuccessStatus: 200, // For legacy browser support
   };
 
-  const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-    // store: ... , // Redis, Memcached, etc. See below.
-    handler: (req, res, next, options) => {
-      res.status(options.statusCode).json({
-        message: `Too many requests from this IP, please try again after ${Math.ceil(
-          options.windowMs / 1000 / 60,
-        )} minutes`,
-      });
-    },
+  // const limiter = rateLimit({
+  //   windowMs: 15 * 60 * 1000, // 15 minutes
+  //   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  //   standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  //   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  //   ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  //   // store: ... , // Redis, Memcached, etc. See below.
+  //   handler: (req, res, next, options) => {
+  //     res.status(options.statusCode).json({
+  //       message: `Too many requests from this IP, please try again after ${Math.ceil(
+  //         options.windowMs / 1000 / 60,
+  //       )} minutes`,
+  //     });
+  //   },
     // keyGenerator: (req, res) => {
     //   const ip = ipKeyGenerator(req.ip, 56);
     //   return `${ip}-${req.path}`;
     // },
-  });
+  // });
 
   app.set("trust proxy", true); // Enable if behind a proxy (e.g., Heroku, AWS ELB)
-  app.use(cors(corsOptions), helmet(), limiter, express.json());
+  app.use(cors(corsOptions), helmet(), express.json());
   app.use("/uploads", express.static(resolve("../uploads/")));
 
   // DB
@@ -56,7 +54,7 @@ async function bootstrap() {
   await connectRedis();
 
   //application routing
-  app.get("/", (req, res) => res.send("Hello World!"));
+  app.get("/", (req, res) => res.send("Hello World! Welcome to Anonify"));
   app.use("/auth", authRouter);
   app.use("/user", userRouter);
   app.use("/message", messageRouter);
